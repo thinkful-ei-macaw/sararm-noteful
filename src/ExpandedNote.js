5f4261e92125e2fe42866568642d5b2f04b6d8f7
Sara R Mills <SaraRMills@gmail.com>
2020-03-10 18:59:30 -0400

    all is well but have not tested much

--- src/ExpandedNote.js
+++ src/ExpandedNote.js
@@ -4,17 +4,33 @@ import React, { Component } from "react";
 class ExpandedNote extends Component {
 
 
-formatNotes = (notes) => {
-  let allNotes = notes.map(item => {
-    return (
-    <li className="note" key={item.id}>
-      <h2>
-        <Link to={'/note'+ item.id}>{item.name}</Link>
-      </h2>
-      <p>Last modified on {this.formatDate(item.modified)}</p>
-      <button>Delete Note</button>
-    </li>
-    )
-  })
-  return allNotes
+formatNotes = () => {
+  let expId = this.props.expId
+  let notes = this.props.notes
+  let expNote = notes.find(item => 
+    item.id === expId)
+  
+  console.log(this.props)
+  return (
+    <li className="note" key={expNote.id}>
+        <h2>
+          {expNote.name}
+        </h2>
+        <p>{expNote.content}</p>
+        <p>Last modified on {this.props.formatDate(expNote.modified)}</p>
+        <button>Delete Note</button>
+      </li>
+  )
 }
+
+  render(){
+    return(
+      <div>
+        {this.formatNotes()}
+      </div>
+    )
+  }
+
+}
+
+export default ExpandedNote
--- src/NoteList.js
+++ src/NoteList.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Note from './Note'
 import FilterNote from './FilterNote'
 import { Link } from 'react-router-dom'
+import ExpandedNote from './ExpandedNote'
 
 
 class NoteList extends Component {
 
-  formatDate(date){
+  formatDate = (date) => {
     let formatted = new Date(date)
     let newTime =  formatted.getMonth()+1 +'/'+ formatted.getDate() +'/'+  formatted.getFullYear()
     return newTime
@@ -14,14 +15,12 @@ class NoteList extends Component {
 
 
   formatNotes = (notes) => {
-    let expId = this.props.match.params.noteId;
     let allNotes = notes.map(item => {
       return ( 
       <li className="note" key={item.id}>
         <h2>
           <Link to={'/note/'+ item.id}>{item.name}</Link>
         </h2>
-        {expId && item.id === expId && <p>{item.content}</p>}
         <p>Last modified on {this.formatDate(item.modified)}</p>
         <button>Delete Note</button>
       </li>
@@ -31,17 +30,25 @@ class NoteList extends Component {
   }
 
   render() {
+    let noteId = this.props.match.params.noteId
+    let folderId = this.props.match.params.folderId
+    console.log('note', noteId, 'folder', folderId)
+    console.log(this.formatDate)
     return(
       <ul>
         {
-          this.props.match.params.folderId ? 
+          noteId ?
+            <ExpandedNote notes={this.props.notes} formatDate={this.formatDate} formatNotes={this.formatNotes} expId={noteId}/>
+        : 
+
+          folderId ? 
             <FilterNote 
               notes={this.props.notes}  filterId={this.props.match.params.folderId} formatDate={this.formatDate} formatNotes={this.formatNotes}/>
         : 
           <Note 
             notes={this.props.notes} formatDate={this.formatDate} formatNotes={this.formatNotes}/>
+        
         }
-
         
       </ul>
     )
